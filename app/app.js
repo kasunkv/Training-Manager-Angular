36640a5dbabcbebbff25c6f3d5e3709c6c07208c
kasunkv <kvkrusl@gmail.com>
2016-12-27 20:03:35 +0530

    Relocated the view for list and courses in the own folder. Uodate the routes. Added 2 new views for abstract views

--- app/app.js
+++ app/app.js
@@ -22,37 +22,47 @@
             .state('courses', {
                 url: '/courses',
                 abstract: true,
-                template: '<ui-view/>'
+                templateUrl: './app/views/courses/course.html'
             })
+                .state('courses.all', {
+                    url: '/all',
+                    templateUrl: './app/views/courses/course.all.html',
+                    controller: 'CourseController'
+                })
                 .state('courses.add', {
                     url: '/add',
-                    templateUrl: './app/views/course.add.html',
+                    templateUrl: './app/views/courses/course.add.html',
                     controller: 'CourseController'
                 })
                 .state('courses.inprogress', {
                     url: '/inprogress',
-                    templateUrl: './app/views/course.inprogress.html',
+                    templateUrl: './app/views/courses/course.inprogress.html',
                     controller: 'CourseController'
                 })
                 .state('courses.completed', {
                     url: '/completed',
-                    templateUrl: './app/views/course.completed.html',
+                    templateUrl: './app/views/courses/course.completed.html',
                     controller: 'CourseController'
                 })
             // Lists Routes
             .state('lists', {
                 url: '/lists',
                 abstract: true, 
-                template: '<ui-view/>'
+                templateUrl: './app/views/lists/lists.html'
             })
                 .state('lists.new', {
                     url: '/new',
-                    templateUrl: './app/views/lists.new.html',
+                    templateUrl: './app/views/lists/lists.new.html',
                     controller: 'ListsController'
                 })
                 .state('lists.all', {
                     url: '/all',
-                    templateUrl: './app/views/lists.all.html',
+                    templateUrl: './app/views/lists/lists.all.html',
+                    controller: 'ListsController'
+                })
+                .state('lists.completed', {
+                    url: '/completed',
+                    templateUrl: './app/views/lists/lists.completed.html',
                     controller: 'ListsController'
                 });
     }    
