64573a7a70768b43fa79611915612ad28ecd707a
kasunkv <kvkrusl@gmail.com>
2016-12-27 22:25:32 +0530

    Created the insert functionality

--- app/controllers/courseController.js
+++ app/controllers/courseController.js
@@ -5,10 +5,30 @@
         .module('trainingManager')
         .controller('CourseController', courseController);
     
-    courseController.$inject = ['$scope'];
+    courseController.$inject = ['$scope', '$state', 'courseFactory' ];
 
-    function courseController($scope) {
-        $scope.message = 'From Course Controller';    
+    function courseController($scope, $state, courseFactory) {
+        $scope.newCourse = {};
+
+        $scope.cancel = function () {
+            $state.go('courses.all');
+        }
+
+        $scope.insert = function () {
+            // Call the API with the course data
+            var tags = $scope.newCourse.tags;
+            $scope.newCourse.tags = tags.split(', ');
+
+            courseFactory.addCourse(
+                $scope.newCourse,
+                function (response, status) {
+                    console.log('success');
+                    $state.go('courses.all');                    
+                },
+                function (error) {
+                    console.error(error);
+                });
+        }
     }
 
 })();
