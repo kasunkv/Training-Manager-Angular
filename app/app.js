4bb218d57fbb517ec8dacaf175dbbc6f5c97c011
kasunkv <kvkrusl@gmail.com>
2016-12-27 19:24:33 +0530

    Added the rest of the states and views for navigation.

--- app/app.js
+++ app/app.js
@@ -14,15 +14,42 @@
                 controller: 'HomeController',
                 templateUrl: './app/views/home.html'
             })
+            // Courses Routes
             .state('courses', {
                 url: '/courses',
                 abstract: true,
                 template: '<ui-view/>'
             })
-            .state('courses.add', {
-                url: '/add',
-                templateUrl: './app/views/course.add.html',
-                controller: 'CourseController'
-            });
+                .state('courses.add', {
+                    url: '/add',
+                    templateUrl: './app/views/course.add.html',
+                    controller: 'CourseController'
+                })
+                .state('courses.inprogress', {
+                    url: '/inprogress',
+                    templateUrl: './app/views/course.inprogress.html',
+                    controller: 'CourseController'
+                })
+                .state('courses.completed', {
+                    url: '/completed',
+                    templateUrl: './app/views/course.completed.html',
+                    controller: 'CourseController'
+                })
+            // Lists Routes
+            .state('lists', {
+                url: '/lists',
+                abstract: true, 
+                template: '<ui-view/>'
+            })
+                .state('lists.new', {
+                    url: '/new',
+                    templateUrl: './app/views/lists.new.html',
+                    controller: 'ListsController'
+                })
+                .state('lists.all', {
+                    url: '/all',
+                    templateUrl: './app/views/lists.all.html',
+                    controller: 'ListsController'
+                });
     }    
 })();
