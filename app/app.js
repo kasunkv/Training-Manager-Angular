3497b73e8109a06f1b7d04b953356e00a35eecd1
kasunkv <kvkrusl@gmail.com>
2016-12-27 19:15:32 +0530

    Added the initial routes and navigation in to the app with the navigation view and other related code.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,28 @@
 (function () {
     'use strict';
 
-    angular.module('trainingManager', ['ngRoute']);
+    angular
+        .module('trainingManager', ['ui.router'])
+        .config(configuration);
+    
+    configuration.$inject = ['$stateProvider'];
+
+    function configuration($stateProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                controller: 'HomeController',
+                templateUrl: './app/views/home.html'
+            })
+            .state('courses', {
+                url: '/courses',
+                abstract: true,
+                template: '<ui-view/>'
+            })
+            .state('courses.add', {
+                url: '/add',
+                templateUrl: './app/views/course.add.html',
+                controller: 'CourseController'
+            });
+    }    
 })();
